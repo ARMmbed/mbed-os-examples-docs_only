145ee559a02de31ba19dd1c7ea61500358addb25
Filip Jagodzinski <filip.jagodzinski@mobica.com>
2018-10-23 17:37:59 +0200

    Move & update EventFlags example from docs repo

---
+++ EventFlags/main.cpp
@@ -0,0 +1,29 @@
+#include "mbed.h"
+
+#define SAMPLE_FLAG1 (1UL << 0)
+#define SAMPLE_FLAG2 (1UL << 9)
+
+EventFlags event_flags;
+
+void worker_thread_fun()
+{
+    printf("Waiting for any flag from 0x%08lx.\r\n", SAMPLE_FLAG1 | SAMPLE_FLAG2);
+    uint32_t flags_read = 0;
+    while (true) {
+        flags_read = event_flags.wait_any(SAMPLE_FLAG1 | SAMPLE_FLAG2);
+        printf("Got: 0x%08lx\r\n", flags_read);
+    }
+}
+
+int main()
+{
+    Thread worker_thread;
+    worker_thread.start(mbed::callback(worker_thread_fun));
+
+    while (true) {
+        wait(1.0);
+        event_flags.set(SAMPLE_FLAG1);
+        wait(0.5);
+        event_flags.set(SAMPLE_FLAG2);
+    }
+}
