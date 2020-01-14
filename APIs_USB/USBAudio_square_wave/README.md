7c96caf68a80d359772f1cd77a8b0d920c635bc8
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2020-01-14 11:03:58 +0100

    Add USBAudio square wave example

---
+++ APIs_USB/USBAudio_square_wave/README.md
@@ -0,0 +1,9 @@
+## USBAudio square wave example
+
+The example outputs an audio square wave over USB.
+
+1. Flash the board, and ensure the target's auxiliary USB is plugged into the PC.
+2. Open a program like Audacity, select microphone(Mbed audio) as an audio source and record sample.
+3. Zoom in the recorded sound and you should see a square wave.
+
+**Note:** You can also route microphone(Mbed audio) to the output device and you should hear a buzz.
---
+++ APIs_USB/USBAudio_square_wave/main.cpp
@@ -0,0 +1,31 @@
+// This example simply generates a square wave.
+// Use a program like Audacity to record and view the square wave, or route microphone(Mbed audio) to the output device.
+#include "mbed.h"
+#include "USBAudio.h"
+#include <math.h>
+
+int16_t square_wave(uint32_t freq_hz, uint16_t amplitude, float time_s)
+{
+    float period = (float)1 / freq_hz;
+    if (fmod(time_s, period) > period / 2) {
+        return amplitude / 2;
+    } else {
+        return -(amplitude / 2);
+    }
+}
+
+int main() {
+    uint32_t tx_freq = 16000;
+    USBAudio audio(true, 8000, 2, tx_freq, 1, 10, 0x7bb8, 0x1112, 0x0100);
+    float cur_time = 0;
+    while (true) {
+        uint16_t samples[64];
+        for (int i = 0; i < 64; i++) {
+            samples[i] = square_wave(100, 5000, cur_time);
+            cur_time += 1.0 / tx_freq;
+        }
+        if (!audio.write((uint8_t *)&samples, sizeof(samples))) {
+            audio.write_wait_ready();
+        }
+    }
+}
