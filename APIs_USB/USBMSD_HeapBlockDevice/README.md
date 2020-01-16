9b9c0467092e8c7d260412f7192a21d655d4aa95
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2020-01-16 13:44:36 +0100

    Add USBMSD heap block device example

---
+++ APIs_USB/USBMSD_HeapBlockDevice/README.md
@@ -0,0 +1,11 @@
+## USBMSD HeapBlockDevice example
+
+This example turns the Mbed board into the mass storage device (HeapBlockDevice).
+
+1. Flash the board.
+2. Reset the target.
+4. USB mass storage drive should appear in your file explorer.
+5. You should be able to access files on this drive.
+
+**Note:** This example requires at least 64 kB of free heap.
+**Note:** Please try to connect board directly to the PC (without USB hub) if you encountered some problems with this example.
---
+++ APIs_USB/USBMSD_HeapBlockDevice/main.cpp
@@ -0,0 +1,36 @@
+#include "mbed.h"
+#include "USBMSD.h"
+#include "FATFileSystem.h"
+
+#define DEFAULT_BLOCK_SIZE  512
+#define HEAP_BLOCK_DEVICE_SIZE (128 * DEFAULT_BLOCK_SIZE)
+
+FATFileSystem heap_fs("heap_fs");
+HeapBlockDevice bd(HEAP_BLOCK_DEVICE_SIZE, DEFAULT_BLOCK_SIZE);
+
+int main() {
+    bd.init();
+
+    FATFileSystem::format(&bd);
+
+    int err = heap_fs.mount(&bd);
+
+    if (err) {
+        printf("%s filesystem mount failed\ntry to reformat device... \r\n", heap_fs.getName());
+        err = heap_fs.reformat(&bd);
+    }
+
+    // If still error, then report failure
+    if(err) {
+        printf("Error: Unable to format/mount the device.\r\n");
+        while(1);
+    }
+
+    USBMSD usb(&bd);
+
+    while(true) {
+        usb.process();
+    }
+
+    return 0;
+}
