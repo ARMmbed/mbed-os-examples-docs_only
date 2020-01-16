26edf0b5c8d66133616e96ffb352c2f1c5427e3e
Maciej Bocianski <maciej.bocianski@gmail.com>
2020-01-16 13:43:30 +0100

    add EventQueue deffering example
    Move example:
    https://os.mbed.com/teams/mbed_example/code/events_ex_1/
    to this repoi
    
    Changed Thread::gettid() to ThisThread::get_id()
    Changed printf to UnbufferedSerial::write to log from ISR

---
+++ APIs_RTOS/EventQueue_ex_1/README.md
@@ -0,0 +1,4 @@
+# EventQueue example
+
+This example shows how to use EventQueue to defer from interrupt context.
+
---
+++ APIs_RTOS/EventQueue_ex_1/main.cpp
@@ -0,0 +1,34 @@
+#include "mbed.h"
+
+DigitalOut led1(LED1);
+InterruptIn sw(SW2);
+EventQueue queue(32 * EVENTS_EVENT_SIZE);
+Thread t;
+
+
+// It's safe to use UnbufferedSerial in ISR context
+UnbufferedSerial console(USBTX, USBRX);
+
+void rise_handler(void) {
+    char buf[64];
+    sprintf(buf, "rise_handler in context %p\n", ThisThread::get_id());
+    console.write(buf, strlen(buf));
+    // Toggle LED
+    led1 = !led1;
+}
+
+void fall_handler(void) {
+    printf("rise_handler in context %p\n", ThisThread::get_id());
+    // Toggle LED
+    led1 = !led1;
+}
+
+int main() {
+    // Start the event queue
+    t.start(callback(&queue, &EventQueue::dispatch_forever));
+    printf("Starting in context %p\r\n", ThisThread::get_id());
+    // The 'rise' handler will execute in IRQ context
+    sw.rise(rise_handler);
+    // The 'fall' handler will execute in the context of thread 't'
+    sw.fall(queue.event(fall_handler));
+}
