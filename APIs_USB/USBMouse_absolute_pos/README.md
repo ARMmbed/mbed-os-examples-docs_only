81e7bc0c26f06ca33afd913373cbfd7b69a6a2e6
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2020-01-20 13:54:31 +0100

    Add USBMouse absolute positioning example

---
+++ APIs_USB/USBMouse_absolute_pos/README.md
@@ -0,0 +1,9 @@
+## USBMouse absolute positioning example
+
+The example emulates mouse over USB port.
+
+1. Flash the board, and ensure the target's auxiliary USB is plugged into the PC.
+2. Reset the target.
+3. The mouse pointer should move on the screen making a cycle.
+
+**Note:** With an absolute mouse, the USBMouse move function will move the mouse to coordinates x, y on the screen.
---
+++ APIs_USB/USBMouse_absolute_pos/main.cpp
@@ -0,0 +1,31 @@
+#include "mbed.h"
+#include "USBMouse.h"
+
+//setup mouse to be absolute
+USBMouse mouse(true, ABS_MOUSE);
+
+#include <math.h>
+
+int main(void)
+{
+  uint16_t x_center = (X_MAX_ABS - X_MIN_ABS)/2;
+  uint16_t y_center = (Y_MAX_ABS - Y_MIN_ABS)/2;
+  uint16_t x_screen = 0;
+  uint16_t y_screen = 0;
+
+  uint32_t x_origin = x_center;
+  uint32_t y_origin = y_center;
+  uint32_t radius = 5000;
+  uint32_t angle = 0;
+
+  while (1)
+  {
+      x_screen = x_origin + cos((double)angle*3.14/180.0)*radius;
+      y_screen = y_origin + sin((double)angle*3.14/180.0)*radius;
+
+      //x_screen, y_screen will be the mouse's position on the screen
+      mouse.move(x_screen, y_screen);
+      angle += 3;
+      ThisThread::sleep_for(10);
+  }
+}
