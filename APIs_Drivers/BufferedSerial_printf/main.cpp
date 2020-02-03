60ca0c2d6799d96e39c9112895b49c3e9c877bb2
Hugues Kamba <hugues.kamba@arm.com>
2020-02-03 15:26:03 +0000

    Add code snippet for UnbufferedSerial and BufferedSerial API documentation

---
+++ APIs_Drivers/BufferedSerial_printf/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020 Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+// Specify different pins to test printing on UART other than the console UART.
+#define TARGET_TX_PIN                                                     USBTX
+#define TARGET_RX_PIN                                                     USBRX
+
+// Create a BufferedSerial object to be used by the system I/O retarget code.
+static BufferedSerial serial_port(TARGET_TX_PIN, TARGET_RX_PIN, 9600);
+
+FileHandle *mbed::mbed_override_console(int fd) {
+    return &serial_port;
+}
+
+int main(void) {
+    // print to the console using the `serial_port` object.
+    printf(
+        "Mbed OS version %d.%d.%d\n",
+        MBED_MAJOR_VERSION,
+        MBED_MINOR_VERSION,
+        MBED_PATCH_VERSION
+    );
+}
