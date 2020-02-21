36a627c549d171b31d3d8d2b546cf8441141993b
Maciej Bocianski <maciej.bocianski@gmail.com>
2020-02-21 10:02:08 +0100

    add rtos basic example
    Move example:
    https://os.mbed.com/teams/mbed_example/code/rtos_basic/
    to this repo
    
    Update example code to latest API's

---
+++ APIs_RTOS/Basic/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "mbed.h"
+ 
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+Thread thread;
+ 
+void led2_thread() {
+    while (true) {
+        led2 = !led2;
+        ThisThread::sleep_for(1000);
+    }
+}
+ 
+int main() {
+    thread.start(led2_thread);
+    
+    while (true) {
+        led1 = !led1;
+        ThisThread::sleep_for(500);
+    }
+}
