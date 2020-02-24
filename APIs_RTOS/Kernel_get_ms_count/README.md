9657cafdb6834858e3796a50301eb225c9c2a95a
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2020-02-24 11:34:19 +0100

    Add Kernel get_ms_count() example.

---
+++ APIs_RTOS/Kernel_get_ms_count/README.md
@@ -0,0 +1,3 @@
+## Kernel Hooks example
+
+The example shows how to use the `Kernel::get_ms_count()` function to read the current RTOS kernel millisecond tick count.
---
+++ APIs_RTOS/Kernel_get_ms_count/main.cpp
@@ -0,0 +1,17 @@
+#include "mbed.h"
+
+int main()
+{
+    // 64-bit time doesn't wrap for half a billion years, at least
+    uint64_t now = Kernel::get_ms_count();
+    
+    // wait a while
+    ThisThread::sleep_for(10);    
+
+    uint64_t later = Kernel::get_ms_count();
+
+    //calculate millisecs elapsed
+    uint64_t elapsed_ms = later - now;
+    
+    printf("Elapsed ms: %u \r\n", (uint32_t)elapsed_ms);
+}
