ce61a77260139b92ea86c0ceb146581539bf665a
Qinghao Shi <Qinghao.Shi@arm.com>
2020-03-31 15:58:42 +0100

    add testing example snippets (#111)

---
+++ Tools_Testing/Greentea_Ex_1/main.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020 Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include "utest/utest.h"
+#include "unity/unity.h"
+#include "greentea-client/test_env.h"
+
+using namespace utest::v1;
+
+// This is how a test case looks
+static control_t simple_test(const size_t call_count)
+{
+    /* test content here */
+    TEST_ASSERT_EQUAL(4, 2 * 2);
+
+    return CaseNext;
+}
+
+utest::v1::status_t greentea_setup(const size_t number_of_cases)
+{
+    // Here, we specify the timeout (60s) and the host test (a built-in host test or the name of our Python file)
+    GREENTEA_SETUP(60, "default_auto");
+
+    return greentea_test_setup_handler(number_of_cases);
+}
+
+// List of test cases in this file
+Case cases[] = {
+    Case("simple test", simple_test)
+};
+
+Specification specification(greentea_setup, cases);
+
+int main()
+{
+    return !Harness::run(specification);
+}
