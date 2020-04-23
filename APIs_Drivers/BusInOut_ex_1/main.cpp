59de2c3335f2f4140675b36179f2b1d4d76ede45
Qinghao Shi <qinghao.shi@arm.com>
2020-04-23 11:06:12 +0100

    fix astyle

--- APIs_Drivers/BusInOut_ex_1/main.cpp
+++ APIs_Drivers/BusInOut_ex_1/main.cpp
@@ -4,17 +4,18 @@
  */
 
 #include "mbed.h"
- 
+
 BusInOut pins(D0, D1, D2); // Change these pins to buttons on your board.
- 
-int main() {
-    while(1) {
+
+int main()
+{
+    while (1) {
         pins.output();
         pins = 0x3;
         ThisThread::sleep_for(1000);
         pins.input();
         ThisThread::sleep_for(1000);
-        if(pins == 0x6) {
+        if (pins == 0x6) {
             printf("Hello!\n");
         }
     }
