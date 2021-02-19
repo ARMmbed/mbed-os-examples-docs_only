b97ae2679a1cedc04451a7687ef03816114bd024
Harrison Mutai <harrison.mutai@arm.com>
2021-02-19 15:15:21 +0000

    Update example timer library calls to use chrono values
    Timer functions now use chrono values, replace deprecated calls to
    library. Replace integer time values with chrono durations.

--- Tutorials_UsingAPIs/Alarm/main.cpp
+++ Tutorials_UsingAPIs/Alarm/main.cpp
@@ -5,8 +5,8 @@
 #include "mbed.h"
 
 // Time constants in seconds
-#define HOUR   60 * 60
-#define MINUTE 60
+std::chrono::hours hour_inc;
+std::chrono::minutes min_inc;
 
 // Globals
 DigitalOut alarm_out(D2, 0);
@@ -19,9 +19,7 @@ InterruptIn sel(BUTTON2);
 
 LowPowerTicker alarm_event;
 
-volatile uint64_t delay        = 0;
-volatile uint8_t  hour_count   = 0;
-volatile uint8_t  min_count    = 0;
+std::chrono::microseconds  delay;
 volatile uint8_t  select_state = 0;
 
 // Timer Callbacks
@@ -48,12 +46,10 @@ void set_time_leds(void)
 void inc_delay(void)
 {
     if (select_state == 0) {
-        delay += HOUR;
-        hour_count++;
+        delay += hour_inc++;
         hour_led = !hour_led;
     } else {
-        delay += MINUTE;
-        min_count++;
+        delay =+ min_inc++;
         min_led = !min_led;
     }
 }
@@ -92,19 +88,19 @@ int main()
 
     // Sleep while waiting for user input to set the desired delay
     while (select_state < 2) {
-        ThisThread::sleep_for(10);
+        ThisThread::sleep_for(10s);
     }
 
     // Once the delay has been input, blink back the configured hours and
     // minutes selected
-    for (uint8_t i = 0; i < hour_count * 2; i++) {
+    for (uint8_t i = 0; i < hour_inc.count() * 2; i++) {
         hour_led = !hour_led;
-        ThisThread::sleep_for(250);
+        ThisThread::sleep_for(250ms);
     }
 
-    for (uint8_t i = 0; i < min_count * 2; i++) {
+    for (uint8_t i = 0; i < min_inc.count() * 2; i++) {
         min_led = !min_led;
-        ThisThread::sleep_for(250);
+        ThisThread::sleep_for(250ms);
     }
 
     // Attach the low power ticker with the configured alarm delay
